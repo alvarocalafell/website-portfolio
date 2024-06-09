acde17b223bb67597b592f2967b5b1e63f173ad2
alvarocalafell <alvarocalafell@gmail.com>
2024-06-08 18:25:03 -0700

    change in data.navLink

--- assets/js/script.js
+++ assets/js/script.js
@@ -135,7 +135,6 @@ for (let i = 0; i < formInputs.length; i++) {
 }
 
 
-
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
@@ -145,7 +144,7 @@ for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
 
     for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+      if (this.dataset.navLink === pages[i].dataset.page) {
         pages[i].classList.add("active");
         navigationLinks[i].classList.add("active");
         window.scrollTo(0, 0);
